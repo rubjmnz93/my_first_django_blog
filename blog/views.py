0c38d2132e82438a8beeff6ab31494bb9ac9571f
rubjmnz93 <rubenjmnz93@gmail.com>
2020-09-19 19:04:08 +0200

    Added template to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(generic.ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-published_date')
+
+
+"""
+def post_list(request):
+    posts = Post.objects.order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+"""
